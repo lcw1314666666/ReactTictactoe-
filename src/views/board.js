8efee4d0a9fdba2b0e04b25242e5cd4cb85cf195
lcw1314666666 <1820566696@qq.com>
2022-03-02 14:43:27 +0800

    添加注释

--- src/views/board.js
+++ src/views/board.js
@@ -14,22 +14,22 @@ class Board extends React.Component {
     }
   }
 
-  handleClick(i) {
-    if (this.state.isVictory || this.state.isFillAll) {
+  handleClick(i) { // 点击空格触发该事件
+    if (this.state.isVictory || this.state.isFillAll) { // 如果已经有一方获胜，或者所有格子都填充完毕直接返回出去
       return
     }
-    const squares = this.state.squares.slice();
-    if (squares[i] === 'X' || squares[i] === 'O') {
+    const squares = this.state.squares.slice(); // 复制棋盘数据
+    if (squares[i] === 'X' || squares[i] === 'O') { // 如果当前点击格子已有数据，则不允许点击，直接返回出去
       return
     }
     squares[i] = this.state.currentSquare;
-    this.setState({squares: squares});
-    this.setState({currentSquare: this.state.currentSquare === 'X' ? 'O' : 'X'})
+    this.setState({squares: squares}); // 给当前点击的格子赋值
+    this.setState({currentSquare: this.state.currentSquare === 'X' ? 'O' : 'X'}) // 改变下一次落子方
     this.judgeSuccess(squares) // 判断一方是否获胜
     this.judgeIsFillAll(squares) // 判断格子是否全部填满
   }
 
-  judgeSuccess(squares) {
+  judgeSuccess(squares) { // 判断输赢函数
     const board = []
     board.push(squares.slice(0, 3))
     board.push(squares.slice(3, 6))
@@ -54,18 +54,18 @@ class Board extends React.Component {
         return index === 2 || index === 4 || index === 6
       }) // 正斜线
     ]
-    result.forEach(item => {
+    result.forEach(item => { // 判断是否有三星连珠
       let count = 0
       for (let i = 0; i < item.length; i ++) {
-        if (item[i] === 'X') {
+        if (item[i] === 'X') { // 如果当前值为X,让结果加一
           count++
         }
-        if (item[i] === 'O') {
+        if (item[i] === 'O') { // 如果当前值为O,让结果减一
           count--
         }
-        if (count === 3 || count === -3) {
+        if (count === 3 || count === -3) { // 如果结果等于3或者是-3就有一方获胜
           this.state.isVictory = true
-          this.setState({victoryName: this.state.currentSquare});
+          this.setState({victoryName: this.state.currentSquare}); // 给获胜方赋值
           window.alert(this.state.currentSquare + '获胜')
           return
         }
@@ -73,14 +73,14 @@ class Board extends React.Component {
     })
   }
 
-  judgeIsFillAll(squares) {
+  judgeIsFillAll(squares) { // 判断格子是否填满
     const result = squares.every(item => {
       return item === 'X' || item === 'O'
     })
     this.state.isFillAll = result
   }
 
-  renderSquare(i) {
+  renderSquare(i) { // 渲染得个格子
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>
   }
 
